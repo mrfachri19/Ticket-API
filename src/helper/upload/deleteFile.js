00251a497f67c1a575a3422958d0e82e46cd8273
fachri <83835862+mrfachri19@users.noreply.github.com>
2021-10-04 11:26:40 +0700

    Update deleteFile.js

--- src/helper/upload/deleteFile.js
+++ src/helper/upload/deleteFile.js
@@ -1,8 +1,12 @@
 const fs = require("fs");
+const path = require("path");
 
 const deleteFile = (filePath) => {
   // eslint-disable-next-line no-console
-  console.log("proses delete", filePath);
+  // console.log("proses delete", filePath);
+  // eslint-disable-next-line no-console
+  // eslint-disable-next-line no-param-reassign
+  path.join(__dirname, "../../../public/uploads/movie", filePath);
   // eslint-disable-next-line no-console
   fs.unlink(filePath, (err) => console.log(err));
   // menggunakan fs.exsistsync
