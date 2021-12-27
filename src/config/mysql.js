9415aeda37d65dad74dff5a5cc00bc788e41605d
mrfachri19 <fachryfachry1997@gmail.com>
2021-12-27 07:19:57 +0700

    deploy

--- src/config/mysql.js
+++ src/config/mysql.js
@@ -1,14 +1,15 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
 const connection = mysql.createConnection({
   // host: "ec2-54-236-24-175.compute-1.amazonaws.com",
   // user: "fw11fachri",
   // password: "heLktD",
   // database: "fw11fachri_ticketing",
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "tiketing",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 connection.connect((error) => {
